2d021e76cc2ab5e2b77a7d8928a2a3afb9f7208d
rishirajreddy <rishirajreddy.07@gmail.com>
2022-02-02 20:28:09 +0530

    1.Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,9 @@
+//solidity version
+pragma solidity ^0.8.1;
+
+contract TodoList {
+    //--taskCount = Static  Variable taskCount
+    //--public =  Read the value taskCount function from TodoList
+    uint public taskCount = 0;
+    
+}
